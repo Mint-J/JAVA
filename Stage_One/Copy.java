b07b7d2712e6b7df8e23ec9d9ae6236c9d51d26a
Mint-J <34620643+Mint-J@users.noreply.github.com>
2018-01-06 18:43:36 +0800

    Add files via upload

---
+++ Stage_One/Copy.java
@@ -0,0 +1,37 @@
+package com.TestDemo_2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Copy {
+	public static void main(String args[]) throws Exception{
+		long start = System.currentTimeMillis();
+		if (args.length != 2){
+			System.out.println("ERROE!");
+			System.exit(1);
+		}
+		File inFile = new File(args[0]);
+		if (!inFile.exists()){
+			System.out.println("Not Exist!");
+			System.exit(1);
+		}
+		File outFile = new File(args[1]);
+		if (!outFile.getParentFile().exists()){
+			outFile.getParentFile().mkdirs();
+		}
+		InputStream input = new FileInputStream(inFile);
+		OutputStream output = new FileOutputStream(outFile);
+		int temp = 0 ;
+		byte data[] = new byte[1024];
+		while ((temp = input.read(data)) != -1){
+			output.write(data, 0, temp);
+		}
+		input.close();
+		output.close();
+		long end = System.currentTimeMillis();
+		System.out.println("Time: " + (end-start));
+	}
+}
---
+++ Stage_One/PrintFunc.java
@@ -0,0 +1,55 @@
+package com.TestDemo_2;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+
+public class PrintFunc {
+	public static void main(String args[]) throws Exception{
+		String s = "abcdefg";
+		InputStream i = new ByteArrayInputStream(s.getBytes());
+		OutputStream o = new ByteArrayOutputStream();
+		int temp = 0;
+		while ((temp = i.read()) != -1){
+			o.write(Character.toUpperCase(temp));
+		}
+		System.out.println(o);
+		i.close();
+		o.close();
+
+		File fileA = new File("C:"+File.separator+"Game"
+				+File.separator+"JAVA"+File.separator+"ml.txt");
+		File fileB = new File("C:"+File.separator+"Game"
+				+File.separator+"ml.txt");
+		InputStream sa = new FileInputStream(fileA);
+		InputStream sb = new FileInputStream(fileB);
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		while ((temp = sa.read()) != -1){
+			output.write(temp);
+		}
+		while ((temp = sb.read()) != -1){
+			output.write(temp);
+		}
+		byte data[] = output.toByteArray();
+		output.close();
+		sa.close();
+		sb.close();
+		System.out.println(new String(data));
+		
+		try {
+			Integer.parseInt("abc");
+		} catch (Exception e){
+			System.err.println(e);
+		}
+		
+		InputStream input = System.in;
+		byte db[] = new byte[1024];
+		System.out.println("Input:");
+		int len = input.read(db);
+		System.out.println(new String(data , 0 , len));
+	}
+}
