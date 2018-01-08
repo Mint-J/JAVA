e3287b8baa19462cacf50f3384e49a4918fa2633
Mint-J <34620643+Mint-J@users.noreply.github.com>
2018-01-08 15:18:04 +0800

    Add files via upload

---
+++ Stage_One/BufferedR_Scanner.java
@@ -0,0 +1,35 @@
+package com.TestDemo_2;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class BufferedR_Scanner {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean flag = true;
+		while (flag) {
+			String s = br.readLine();
+			if (s.matches("\\d{1,3}")) {
+				System.out.println("Age: " + Integer.parseInt(s));
+				flag = false;
+			} else {
+				System.out.println("ERROR");
+			}
+		}
+		
+		Scanner scan = new Scanner(new FileInputStream(new File("C:"+File.separator+"Game"
+				+File.separator+"JAVA"+File.separator+"ml.txt")));
+		scan.useDelimiter("\n");
+		if (scan.hasNext()){
+			System.out.println(scan.next());
+		}
+		scan.close();
+		
+	}
+
+}
